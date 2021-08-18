0a309348590c0b274fc5b9f6954290427abe6425
Sergio Lopez <slopezjur@users.noreply.github.com>
2021-08-18 20:45:52 +0200

    add testcore module

---
+++ testcore/src/main/java/com/sergiolopez/testcore/BaseTest.kt
@@ -0,0 +1,16 @@
+package com.sergiolopez.testcore
+
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Rule
+
+@ExperimentalCoroutinesApi
+open class BaseTest {
+
+    @get:Rule
+    var coroutinesTestRule = CoroutineTestRule()
+
+    fun runBlocking(block: suspend TestCoroutineScope.() -> Unit) =
+        this.coroutinesTestRule.testDispatcher.runBlockingTest { block() }
+}
